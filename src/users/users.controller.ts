9df0b528c6c836f48d426bf34468b62dec4c8e03
fbarsi <francogbarsi@gmail.com>
2025-10-14 04:04:58 -0300

    fixing imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Body, UseGuards, Request, SetMetadata } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
